b12116885151ba8f19a1fab0ca519eff58fa66c9
Anton Schwarz <aschwarz@Antons-MacBook-Air.local>
2024-10-29 11:27:33 +0900

    Basic stuff works now for my Lazy.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,10 @@
-require("anton.remap")
-print("hello")
+-- Plugins
+require("config.lazy")
+
+-- Config
+require("config.remap")
+
+-- Colourscheme
+vim.o.termguicolors = true
+vim.o.background = 'light'
+vim.cmd.colorscheme 'solarized'
